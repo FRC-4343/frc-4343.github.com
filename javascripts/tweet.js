7696ff9eb749f5ccf78b3ff42aedcde00971b9ab
Indiv0 <indiv0soft@gmail.com>
2012-11-21 21:31:50 -0500

    Added second (robot) twitter feed

--- javascripts/tweet.js
+++ javascripts/tweet.js
@@ -1,5 +1,18 @@
 jQuery(function($){
-  $(".tweet").tweet({
+  $(".robot").tweet({
+    username: "MaxTheRobot",
+    join_text: "auto",
+    avatar_size: 32,
+    count: 4,
+    auto_join_text_default: ": ", 
+    auto_join_text_ed: ": ",
+    auto_join_text_ing: ": ",
+    auto_join_text_reply: ": ",
+    auto_join_text_url: ": ",
+    loading_text: " loading tweets... "
+  });
+  
+  $(".stmax").tweet({
     username: "StMaxFRC",
     join_text: "auto",
     avatar_size: 32,
