d3542bb10d3f8b9332a045078c5e6796939dd0b7
Indiv0 <nikitapekin@gmail.com>
2012-11-22 14:55:59 -0500

    Merged twitter feeds

--- javascripts/tweet.js
+++ javascripts/tweet.js
@@ -1,27 +1,11 @@
 jQuery(function($){
-  $(".robot").tweet({
-    username: "MaxTheRobot",
+  $(".tweet").tweet({
+    username: "tcmalone14",
+    list: "robotics",
     join_text: "auto",
     avatar_size: 32,
-    count: 4,
-    auto_join_text_default: ": ", 
-    auto_join_text_ed: ": ",
-    auto_join_text_ing: ": ",
-    auto_join_text_reply: ": ",
-    auto_join_text_url: ": ",
-    loading_text: " loading tweets... "
-  });
-  
-  $(".stmax").tweet({
-    username: "StMaxFRC",
-    join_text: "auto",
-    avatar_size: 32,
-    count: 4,
-    auto_join_text_default: ": ", 
-    auto_join_text_ed: ": ",
-    auto_join_text_ing: ": ",
-    auto_join_text_reply: ": ",
-    auto_join_text_url: ": ",
-    loading_text: " loading tweets... "
+    count: 8,
+    loading_text: " loading tweets... ",
+    refresh_interval: 60
   });
 });
