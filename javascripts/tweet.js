1b9114df213c434dfd24a01202ba0bd8f3157ec1
Indiv0 <nikitapekin@gmail.com>
2012-11-22 15:02:20 -0500

    Updated twitter feed text size

--- javascripts/tweet.js
+++ javascripts/tweet.js
@@ -6,6 +6,11 @@ jQuery(function($){
     avatar_size: 32,
     count: 8,
     loading_text: " loading tweets... ",
-    refresh_interval: 60
+    refresh_interval: 60,
+    auto_join_text_default: ": ", 
+    auto_join_text_ed: ": ",
+    auto_join_text_ing: ": ",
+    auto_join_text_reply: ": ",
+    auto_join_text_url: ": ",
   });
 });
